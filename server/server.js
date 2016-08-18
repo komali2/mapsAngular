57bedffec368984c038a2ff986fe40b7cec46eb3
komali2 <rogersjcaleb@gmail.com>
2016-08-17 19:42:42 -0700

    core: changed to not require server

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-var clientPath = path.join(__dirname + '/../client');
+var clientPath = path.join(__dirname + '/');
 app.use(express.static(clientPath));
 
 var server = app.listen(process.env.PORT || 8080, ()=>{
