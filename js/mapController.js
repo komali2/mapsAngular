9904f118932a344f169a802796be2e6164d0e273
komali2 <rogersjcaleb@gmail.com>
2016-08-18 17:31:49 -0700

    dev: tweaked eslint

--- js/mapController.js
+++ js/mapController.js
@@ -4,7 +4,7 @@ angular.module('mapApp')
         $scope.center = 'SOMA';
         //$scope.map = new google.maps.Map(document.getElementById('map'), mapFactory.mapOptions[$scope.center]);
         $scope.map = mapFactory.init(document.getElementById('map'), $scope.center);
-        const service = new google.maps.places.PlacesService($scope.map);
+        //const service = new google.maps.places.PlacesService($scope.map);
 
         $scope.markers = [];
         
