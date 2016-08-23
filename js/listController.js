5f6582f2f74662d0a74758e41cafdcabac24a2f8
komali2 <rogersjcaleb@gmail.com>
2016-08-22 21:49:39 -0700

    dev: cleaned up and commented list

--- js/listController.js
+++ js/listController.js
@@ -1,13 +1,12 @@
 angular.module('mapApp')
-    .factory('listFactory', [()=>{
+    .factory('listFactory', [() => {
 
     }])
-    .controller('listController', ['$scope', 'mapFactory', ($scope, mapFactory)=>{
-        // $scope.$watch('searchData.results', (newVal, oldVal)=>{
-        //     console.log('list saw the change', newVal);
-        // });
-        $scope.openInfoWindow = function(e, selectedMarker){
+    .controller('listController', ['$scope', 'mapFactory', ($scope, mapFactory) => {
+
+        // Map could do this on its own, but this is more MVC friendly
+        // Could also follow a Backbone model and use emitters / listeners
+        $scope.openInfoWindow = function (e, selectedMarker) {
             mapFactory.openInfoWindow(e, selectedMarker);
         };
-        
     }]);
