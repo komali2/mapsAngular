b5d0473714de1f4d92d5c1f4aef5c3fd3bf21dd3
komali2 <rogersjcaleb@gmail.com>
2016-08-22 22:04:44 -0700

    dev: swapped list to 2 spaces

--- js/listController.js
+++ js/listController.js
@@ -1,12 +1,12 @@
 angular.module('mapApp')
-    .factory('listFactory', [() => {
+  .factory('listFactory', [() => {
 
-    }])
-    .controller('listController', ['$scope', 'mapFactory', ($scope, mapFactory) => {
+  }])
+  .controller('listController', ['$scope', 'mapFactory', ($scope, mapFactory) => {
 
-        // Map could do this on its own, but this is more MVC friendly
-        // Could also follow a Backbone model and use emitters / listeners
-        $scope.openInfoWindow = function (e, selectedMarker) {
-            mapFactory.openInfoWindow(e, selectedMarker);
-        };
-    }]);
+    // Map could do this on its own, but this is more MVC friendly
+    // Could also follow a Backbone model and use emitters / listeners
+    $scope.openInfoWindow = function (e, selectedMarker) {
+      mapFactory.openInfoWindow(e, selectedMarker);
+    };
+  }]);
