9ed9c771a7b67a28c53fe618fa485d4e953aa198
komali2 <rogersjcaleb@gmail.com>
2016-08-22 22:02:42 -0700

    dev: swapped search to 2 spaces

--- js/searchController.js
+++ js/searchController.js
@@ -1,51 +1,51 @@
 angular.module('mapApp')
-    .factory('searchFactory', [()=>{
-        let api = {};
-        let service;
+  .factory('searchFactory', [() => {
+    let api = {};
+    let service;
 
-        // This creates the Google Maps Search Service
-        api.init = function(map){
-            service = new google.maps.places.PlacesService(map);
-            return service;
-        };
+    // This creates the Google Maps Search Service
+    api.init = function (map) {
+      service = new google.maps.places.PlacesService(map);
+      return service;
+    };
 
-        // Example request:
-        // var request = {
-        //     location: google.maps.LatLng,
-        //     radius: '500',
-        //     keyword: 'something'
-        // };
-        api.search = function(request, callback){
-            service.nearbySearch(request, callback);
-        };
+    // Example request:
+    // var request = {
+    //     location: google.maps.LatLng,
+    //     radius: '500',
+    //     keyword: 'something'
+    // };
+    api.search = function (request, callback) {
+      service.nearbySearch(request, callback);
+    };
 
-        // Called on opening a modal to get more details on given location
-        // PlaceId can only be acquired from a previous search
-        api.getDetails = function(placeId, cb){
-            service.getDetails({placeId}, (place)=>{
-                cb(place);
-            });
-        };
+    // Called on opening a modal to get more details on given location
+    // PlaceId can only be acquired from a previous search
+    api.getDetails = function (placeId, cb) {
+      service.getDetails({ placeId }, (place) => {
+        cb(place);
+      });
+    };
 
-        return api;
-    }])
-    .controller('searchController', ['$scope', 'searchFactory', ($scope, searchFactory)=>{
-        $scope.search = function(keyword){
-            let request = {
-                location: $scope.location,
-                radius: '500',
-                keyword: keyword
-            };
+    return api;
+  }])
+  .controller('searchController', ['$scope', 'searchFactory', ($scope, searchFactory) => {
+    $scope.search = function (keyword) {
+      let request = {
+        location: $scope.location,
+        radius: '500',
+        keyword: keyword
+      };
 
-            searchFactory.search(request, (result)=>{
-                $scope.$emit('search');
-                $scope.searchData.results = result;
-                
-                // This allows flexbox to function without hardcoding a width until it becomes necessary
-                angular.element(document.querySelector('#listArea')).addClass('widthSet');
+      searchFactory.search(request, (result) => {
+        $scope.$emit('search');
+        $scope.searchData.results = result;
 
-                // Necessary to tell Angular to update the parent scope
-                $scope.$apply();
-            });
-        };
-    }]);
+        // This allows flexbox to function without hardcoding a width until it becomes necessary
+        angular.element(document.querySelector('#listArea')).addClass('widthSet');
+
+        // Necessary to tell Angular to update the parent scope
+        $scope.$apply();
+      });
+    };
+  }]);
