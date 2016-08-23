8e7a60894595059f169134b976f17d55eb6fb7a5
komali2 <rogersjcaleb@gmail.com>
2016-08-22 22:05:13 -0700

    dev: swapped main to 2 spaces

--- js/index.js
+++ js/index.js
@@ -3,38 +3,38 @@
 // Main App Declaration
 // ngAnimate only dependency - needed to have Angular attach animation classes
 var app = angular.module('mapApp', ['ngAnimate'])
-    // Parent controller, highest scope in this app
-    .controller('mainController', [
-        '$scope', 
-        'mapFactory', 
-        'searchFactory', 
-        ($scope, mapFactory, searchFactory)=>{
-            
-            // Declaring on $scope here means that child $scopes will inherent, and can modify, 
-            // and can thus see their siblings' changes
-            $scope.center = new google.maps.LatLng(37.779353, -122.398030);
-            $scope.location = new google.maps.LatLng(37.779353, -122.398030);
-            $scope.markers = [];
-            $scope.map = mapFactory.init(document.getElementById('map'), $scope.center);
-            $scope.service = searchFactory.init($scope.map);
+  // Parent controller, highest scope in this app
+  .controller('mainController', [
+    '$scope',
+    'mapFactory',
+    'searchFactory',
+    ($scope, mapFactory, searchFactory) => {
 
-            // Nesting objects is necessary in Angular, otherwise childscopes will create new properties,
-            // rather than modify existing ones on the parent
-            $scope.searchData = {results:[]};
+      // Declaring on $scope here means that child $scopes will inherent, and can modify, 
+      // and can thus see their siblings' changes
+      $scope.center = new google.maps.LatLng(37.779353, -122.398030);
+      $scope.location = new google.maps.LatLng(37.779353, -122.398030);
+      $scope.markers = [];
+      $scope.map = mapFactory.init(document.getElementById('map'), $scope.center);
+      $scope.service = searchFactory.init($scope.map);
 
-            // Angular listener catches a child "emit" event
-            // This one updates location, another way to pass scope changes up the inheritance tree
-            $scope.$on('changedLocation', (event, location)=>{
-                $scope.location = location;
-                $scope.$apply();
-            });
+      // Nesting objects is necessary in Angular, otherwise childscopes will create new properties,
+      // rather than modify existing ones on the parent
+      $scope.searchData = { results: [] };
 
-            // Clears markers off the map and resets the markers array
-            $scope.$on('search', ()=>{
-                for(let i = 0; i < $scope.markers.length; i++){
-                    $scope.markers[i].setMap(null);
-                }
-                $scope.markers = [];
-            });
+      // Angular listener catches a child "emit" event
+      // This one updates location, another way to pass scope changes up the inheritance tree
+      $scope.$on('changedLocation', (event, location) => {
+        $scope.location = location;
+        $scope.$apply();
+      });
 
-        }]);
+      // Clears markers off the map and resets the markers array
+      $scope.$on('search', () => {
+        for (let i = 0; i < $scope.markers.length; i++) {
+          $scope.markers[i].setMap(null);
+        }
+        $scope.markers = [];
+      });
+
+    }]);
