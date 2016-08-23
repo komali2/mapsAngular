282ca0f9bb6d799d0e6fe5247ac67c6910fb9566
komali2 <rogersjcaleb@gmail.com>
2016-08-22 22:00:23 -0700

    dev: cleaned and commented search

--- js/searchController.js
+++ js/searchController.js
@@ -1,43 +1,50 @@
 angular.module('mapApp')
     .factory('searchFactory', [()=>{
         let api = {};
-        var service;
+        let service;
+
+        // This creates the Google Maps Search Service
         api.init = function(map){
             service = new google.maps.places.PlacesService(map);
             return service;
         };
 
-        //example request:
+        // Example request:
         // var request = {
         //     location: google.maps.LatLng,
         //     radius: '500',
-        //     type: 'store',
         //     keyword: 'something'
         // };
         api.search = function(request, callback){
             service.nearbySearch(request, callback);
-            
         };
+
+        // Called on opening a modal to get more details on given location
+        // PlaceId can only be acquired from a previous search
         api.getDetails = function(placeId, cb){
-            service.getDetails({placeId}, (place, status)=>{
+            service.getDetails({placeId}, (place)=>{
                 cb(place);
             });
         };
-        return api;
 
+        return api;
     }])
     .controller('searchController', ['$scope', 'searchFactory', ($scope, searchFactory)=>{
-        $scope.search = function(keyword, type){
+        $scope.search = function(keyword){
             let request = {
                 location: $scope.location,
                 radius: '500',
-                type: type ? type : undefined,
                 keyword: keyword
             };
+
             searchFactory.search(request, (result)=>{
                 $scope.$emit('search');
                 $scope.searchData.results = result;
+                
+                // This allows flexbox to function without hardcoding a width until it becomes necessary
                 angular.element(document.querySelector('#listArea')).addClass('widthSet');
+
+                // Necessary to tell Angular to update the parent scope
                 $scope.$apply();
             });
         };
