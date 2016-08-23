a56525a802332e152852977e4f75dced7f9ddd72
komali2 <rogersjcaleb@gmail.com>
2016-08-22 22:30:01 -0700

    dev: swapped tests to 2 spaces

--- tests/map.spec.js
+++ tests/map.spec.js
@@ -1,44 +1,41 @@
-describe('mapFactory', ()=>{
-    var mapFactory;
-    var map;
-    var infoWindow;
+describe('mapFactory', () => {
+  var mapFactory;
+  var map;
+  var infoWindow;
 
+  var location = {
+    zoom: 14,
+    center: new google.maps.LatLng(37.779353, -122.398030),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
 
-    var location = {
-        zoom: 14,
-        center: new google.maps.LatLng(37.779353, -122.398030),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-
-
-
-    beforeEach(()=>{
-        module('mapApp');
-        inject(($injector)=>{
-            mapFactory = $injector.get('mapFactory');
-        });
+  beforeEach(() => {
+    module('mapApp');
+    inject(($injector) => {
+      mapFactory = $injector.get('mapFactory');
     });
+  });
 
 
-    it('should be defined factory', ()=>{
-        expect(mapFactory).toBeDefined();
-    });
+  it('should be defined factory', () => {
+    expect(mapFactory).toBeDefined();
+  });
 
-    it('should init', ()=>{
-        expect(mapFactory.init).toBeDefined();
-        var element = document.createElement('div');
-        element.classList.add('map');
-        map = mapFactory.init(element, location);
-        expect(map instanceof google.maps.Map).toBe(true);
-    });
+  it('should init', () => {
+    expect(mapFactory.init).toBeDefined();
+    var element = document.createElement('div');
+    element.classList.add('map');
+    map = mapFactory.init(element, location);
+    expect(map instanceof google.maps.Map).toBe(true);
+  });
 
-    it('should have a createMarker function', ()=>{
-        expect(mapFactory.createMarker).toBeDefined();
-    });
+  it('should have a createMarker function', () => {
+    expect(mapFactory.createMarker).toBeDefined();
+  });
 
-    it('should have a openInfoWindow function', ()=>{
-        expect(mapFactory.openInfoWindow).toBeDefined();
-    });
+  it('should have a openInfoWindow function', () => {
+    expect(mapFactory.openInfoWindow).toBeDefined();
+  });
 
 
 });
--- tests/search.spec.js
+++ tests/search.spec.js
@@ -1,22 +1,23 @@
-describe('searchFactory', ()=>{
-    beforeEach(()=>{
-        module('mapApp');
-        inject(($injector)=>{
-            searchFactory = $injector.get('searchFactory');
-        });
+describe('searchFactory', () => {
+  var searchFactory;
+  beforeEach(() => {
+    module('mapApp');
+    inject(($injector) => {
+      searchFactory = $injector.get('searchFactory');
     });
+  });
 
-    it('should exist', ()=>{
-        expect(searchFactory).toBeDefined();
-    });
+  it('should exist', () => {
+    expect(searchFactory).toBeDefined();
+  });
 
-    it('should have a search function', ()=>{
-        expect(searchFactory.search).toBeDefined();
-    });
+  it('should have a search function', () => {
+    expect(searchFactory.search).toBeDefined();
+  });
 
-    it('should have a getDetails function', ()=>{
-        expect(searchFactory.getDetails).toBeDefined();
-    });
+  it('should have a getDetails function', () => {
+    expect(searchFactory.getDetails).toBeDefined();
+  });
 
-    
+
 });
